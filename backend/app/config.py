6bde78d45a1be1ddcc39eed1dd4930c9d9bab149
bevin-creator <fadhilibevin13@gmail.com>
2026-06-25 15:22:55 +0300

    Configured application environment settings using pydantic settings

---
+++ backend/app/config.py
@@ -0,0 +1,25 @@
+# App configuration using pydatic setting
+# Loads values from .env file
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    #App metadata
+    APP_NAME: str = "Visitors Management System"
+    DEBUG: bool = false
+
+    #Database - SQLAlchemy connection string format
+    DATABASE_URL: str ="postgresql://postgres:postgres@localhost:5432/visitor_management"
+
+    #Authentication (JWT)
+    SECRET_KEY: str = "enter secure secret key"
+    ALGORITHM: str = "HS256" #symmetric
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    #CORS for frontend origin
+    CORSE_ORIGIN:list[str]= ["http://localhost:5500", "http://127.0.0.1:5500"]
+
+    class Config:
+        env_file=".env"
+
+settings=Settings()
